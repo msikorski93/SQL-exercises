03126c60bae66680e950ce9c79df9c56db1f02ab
msikorski93 <45270023+msikorski93@users.noreply.github.com>
2022-12-05 23:48:18 +0100

    Add files via upload

--- sql_practice.sql
+++ sql_practice.sql
@@ -97,7 +97,7 @@ ORDER BY year ASC;
 
 --19. Show unique first names from the patients table which only occurs once in the list.
 
---For example, if two or more people are named 'John' in the first_name column then don't include their name in the --output list. If only 1 person is named 'Leo' then include them in the output.
+--For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
 
 SELECT DISTINCT first_name FROM patients
 GROUP BY first_name
