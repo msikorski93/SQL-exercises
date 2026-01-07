df4763f50ac19c80c2685a83d9c6d4ef33555474
msikorski93 <45270023+msikorski93@users.noreply.github.com>
2026-01-07 16:17:31 +0100

    Add files via upload

--- sql_window_functions.sql
+++ sql_window_functions.sql
@@ -214,7 +214,7 @@ SELECT
 name,
 weight,
 breed,
-COALESCE(CAST(LEAD(weight, 1) OVER (PARTITION BY breed ORDER BY weight) as VARCHAR), 'fattest cat') AS next_heaviest
+COALESCE(CAST(LEAD(weight, 1) OVER (PARTITION BY breed ORDER BY weight) AS VARCHAR), 'fattest cat') AS next_heaviest
 FROM cats
 ORDER BY weight;
 
